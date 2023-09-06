3380c70a9e64859bed55eea154c891913465270b
martin-kloeppner <martin.kloeppner@lotto24.de>
2023-09-06 02:05:00 +0000

    Web Worker Mock

--- src/app/customer/user.service.ts
+++ src/app/customer/user.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
 import { Observable, interval, of, switchMap } from 'rxjs';
 
 export type User = {
@@ -6,18 +7,18 @@ export type User = {
   profileIcon: string;
 }
 
-const martin: User = {
-  id: 1,
-  profileIcon: 'https://media.licdn.com/dms/image/C4D03AQFj2D9iDa7oJA/profile-displayphoto-shrink_400_400/0/1516997069619?e=1699488000&v=beta&t=1Rev1nSyFTTO8tvdhjTyBo532QrRt9Cb7yT4Q_Z7rnA'
-}
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+
+  private httpClient = inject(HttpClient);
+
   // State
-  public users$: Observable<User[]> = interval(4000).pipe(
-    switchMap(() => of([martin]))
-  );
+  public get users$(): Observable<User[]> {
+    return this.httpClient.get<User[]>('/users');
+  }
 
 }
